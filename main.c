5af1fce1ad8c43c9e3cc64e06b59b6e75120ad75
muff1n0 <tchen51@stuy.edu>
2024-11-26 14:50:10 -0500

    finish lab

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 int main() {
   printf("%d about to create 2 child processes\n", getpid());
@@ -15,8 +17,29 @@ int main() {
   else if (p > 0) {
     p1 = fork();
   }
+  int rng;
+
   if (p1 == 0) {
     srand(time(NULL) + getpid());
-    printf("%d %dsec\n", getpid(), (rand() + 1) % 6);
+    rng = rand() % 5 + 1;
+    printf("%d %dsec\n", getpid(), rng);
+    sleep(rng);
+    printf("%d finished after %dsec\n", getpid(), rng);
+    return rng;
   }
+  if (p == 0) {
+    srand(time(NULL) + getpid());
+    rng = rand() % 5 + 1;
+    printf("%d %dsec\n", getpid(), rng);
+    sleep(rng);
+    printf("%d finished after %dsec\n", getpid(), rng);
+    return rng;
+  }
+  if (p > 0) {
+    int status;
+    int id = wait(&status);
+    printf("Main Process %d is done. Child %d slept for %dsec\n", getpid(), id, WEXITSTATUS(status));
+  }
+
+
 }
